8d6308cb0c3311dca31365f0fcd30312145c1fc5
jagadhis <jwaran78@gmail.com>
2022-11-10 22:32:42 +0530

    Bot Messages

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js')
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        // ...
+    ]
+})
+
+client.on('ready',() => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+})
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
+  });
+
+client.login(process.env.CLIENT_TOKEN);
