0e8d28862522a661b41954b1643b6023eaa0d836
jagadhis <jwaran78@gmail.com>
2022-11-11 12:28:33 +0530

    Updated packages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config({path: "./vars/.env"});
 const { Client, GatewayIntentBits } = require('discord.js')
+const axios = require('axios');
 
 
 const client = new Client({
@@ -11,6 +12,9 @@ const client = new Client({
 
     ]
 })
+let interval;
+
+
 
 client.on('ready',() => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -20,13 +24,25 @@ client.on('ready',() => {
 
 const prefix = "!";
 
-client.on("messageCreate", function(message) {
+client.on("messageCreate", async function(message) {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
   const commandBody = message.content.slice(prefix.length);
   const args = commandBody.split(' ');
   const command = args.shift().toLowerCase();
+  if(command === "meme"){
+    message.channel.send("Here's your meme!");
+      const img = await getMeme();
+      message.channel.send(img);
+  }
+  else if (command === "eye"){
+    message.channel.send("You are now subscribed to eye reminders.");
+       interval = setInterval (function () {
+        message.channel.send("Please take an eye break now!")
+        .catch(console.error); 
+      }, 3600000); 
+  }
 
   if (command === "ping") {
     const timeTaken = Date.now() - message.createdTimestamp;
@@ -39,5 +55,12 @@ client.on("messageCreate", function(message) {
     message.reply(`The sum of all the arguments you provided is ${sum}!`);
   }
 });
+
+async function getMeme(){
+  const res = await axios.get('https://memeapi.pythonanywhere.com/');
+  console.log(res.data)
+  return res.data.memes[0].url;
+}
+
 
 client.login(process.env.CLIENT_TOKEN);
