0628bb182d3c348711b7fa0e0627f2d58afd8767
jagadhis <jwaran78@gmail.com>
2022-11-11 12:03:00 +0530

    Bot worked

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
+require('dotenv').config({path: "./vars/.env"});
 const { Client, GatewayIntentBits } = require('discord.js')
+
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        // ...
+        GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+
     ]
 })
 
@@ -13,10 +17,27 @@ client.on('ready',() => {
 
 })
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-  });
+
+const prefix = "!";
+
+client.on("messageCreate", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+  }
+
+  else if (command === "sum") {
+    const numArgs = args.map(x => parseFloat(x));
+    const sum = numArgs.reduce((counter, x) => counter += x);
+    message.reply(`The sum of all the arguments you provided is ${sum}!`);
+  }
+});
 
 client.login(process.env.CLIENT_TOKEN);
